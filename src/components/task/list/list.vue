9332899fa2458a94c07b860f8f479a206145deae
litianhang <litianhang@bmzymtr.com>
2017-10-26 13:13:07 +0800

    list.vue demo

--- src/components/task/list/list.vue
+++ src/components/task/list/list.vue
@@ -1,11 +1,34 @@
 <template>
   <div>
-    list
+    <span v-for="(seen, key) in seens" :key="seen.id">
+      <p>{{ key }}</p><p>{{ seen }}</p>
+    </span>
+    <div @click="dianji">点击{{ moren }}</div>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      seens: [
+        {
+          id: 1,
+          name: '李天航'
+        },
+        {
+          id: 2,
+          name: '卢晋'
+        }
+      ],
+      moren: '默认'
+    }
+  },
+  methods: {
+    dianji () {
+      this.moren = '此案次'
+    }
+  }
 }
 </script>
 
