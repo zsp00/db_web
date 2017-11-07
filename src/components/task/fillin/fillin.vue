e7f4922d3c6c607a423970f8a7831705b44fae2d
LuJin <345526407@qq.com>
2017-11-07 10:10:54 +0800

    ''

--- src/components/task/fillin/fillin.vue
+++ src/components/task/fillin/fillin.vue
@@ -8,5 +8,4 @@
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
- 
 </style>
--- src/components/task/list/list.vue
+++ src/components/task/list/list.vue
@@ -1,9 +1,32 @@
 <template>
-  <div>
-    <span v-for="(seen, key) in seens" :key="seen.id">
-      <p>{{ key }}</p><p>{{ seen }}</p>
-    </span>
-    <div @click="dianji">点击{{ moren }}</div>
+  <div class="box">
+    <div class="list-head">
+      <router-link to="/process/process-add">
+        <el-button type="primary">创建任务</el-button>
+      </router-link>
+      <el-button type="primary">删除</el-button>
+      <el-button type="primary">下发任务</el-button>
+    </div>
+    <div class="list-body">
+      <el-table ref="multipleTable" v-loading="loading" :data="processList" tooltip-effect="dark" max-height="700" style="width: 100%" border @selection-change="_handleSelectionChange">
+        <el-table-column type="selection" width="50" align="center"></el-table-column>
+        <el-table-column prop="name" label="流程名称"></el-table-column>
+        <el-table-column prop="dept" label="所属部门" width="280" show-overflow-tooltip></el-table-column>
+        <el-table-column prop="level" label="流程步骤数" width="110"></el-table-column>
+        <el-table-column prop="creator" label="创建者" width="100"></el-table-column>
+        <el-table-column prop="createTime" label="创建时间" width="175"></el-table-column>
+        <el-table-column prop="lastModifier" label="最后一次修改人" width="140"></el-table-column>
+        <el-table-column prop="updateTime" label="最后一次修改时间" width="175"></el-table-column>
+        <el-table-column prop="options" label="操作" width="80">
+          <template slot-scope="scope">
+            <router-link :to="'/process/process-edit/' + scope.row.id">
+              <i class="el-icon-edit"></i>
+            </router-link>
+            <i class="el-icon-delete" @click="_del(scope.row.id)"></i>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
@@ -11,26 +34,46 @@
 export default {
   data () {
     return {
-      seens: [
-        {
-          id: 1,
-          name: '李天航'
-        },
-        {
-          id: 2,
-          name: '卢晋'
-        }
-      ],
-      moren: '默认'
+      multipleSelection: [],                         // 多选记录
+      processList: [],                               // 流程列表
+      loading: false                                  // 加载中
     }
   },
+  mounted () {
+  },
   methods: {
-    dianji () {
-      this.moren = '此案次'
+    _edit (pId) {
+      console.log('edit   :' + pId)
+    },
+    _del (pId) {
+      console.log('del   :' + pId)
+    },
+    _toggleSelection (rows) {
+      if (rows) {
+        rows.forEach(row => {
+          this.$refs.multipleTable.toggleRowSelection(row)
+        })
+      } else {
+        this.$refs.multipleTable.clearSelection()
+      }
+    },
+    _handleSelectionChange (val) {
+      this.multipleSelection = val
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
+.box {
+  margin: 20px;
+}
+.list-body {
+  margin-top: 20px;
+}
+.list-body i {
+  margin-right: 5px;
+  cursor: pointer;
+}
 </style>
+
