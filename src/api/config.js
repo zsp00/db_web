2af72585d23a7658cc28223b6744c1c2da6043c3
litianhang <litianhang@bmzymtr.com>
2017-12-11 20:41:12 +0800

    解决冲突

--- src/api/config.js
+++ src/api/config.js
@@ -1,13 +1,8 @@
 export const ERR_OK = 1
 
 export const DB_API = {
-<<<<<<< HEAD
   hostname: 'dev.db.com/admin',
   // hostname: '192.168.251.50/api',
-=======
-  // hostname: 'dev.db.com/admin',
-  hostname: 'tss.bjdt.com/api',
->>>>>>> 23287f36800983a79a446ea57093278d3cca2c2a
   protocol: 'http',
   headers: {
     'X-Requested-with': 'XMLHttpRequest'
