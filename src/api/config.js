836228cbf4b613706e4d14c27d7dfd7a97d64155
LuJin <345526407@qq.com>
2017-12-18 11:34:58 +0800

    '修改bug'

--- src/api/config.js
+++ src/api/config.js
@@ -1,8 +1,8 @@
 export const ERR_OK = 1
 
 export const DB_API = {
-  // hostname: 'dev.db.com/admin',
-  hostname: 'tss.bjdt.com/api',
+  hostname: 'dev.db.com/admin',
+  // hostname: 'tss.bjdt.com/api',
   protocol: 'http',
   headers: {
     'X-Requested-with': 'XMLHttpRequest'
